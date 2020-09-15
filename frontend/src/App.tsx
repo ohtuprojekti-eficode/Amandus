fa6e2e9824b273d1bb1e8309776237992229ea3c
guotin <eero.kauhanen@helsinki.fi>
2020-09-15 17:54:59 +0300

    Start listing view building

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import ListView from './components/ListView'
+
+interface Tree {
+    path: string
+}
 
 const App = () => {
-    return <div>hello WEVC</div>
+    const [files, setFiles] = useState<string[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const commits = await axios(
+                'https://api.github.com/repos/ohtuprojekti-eficode/robot-test-files/commits'
+            )
+
+            const commitTreeURL: string = commits.data[0].commit.tree.url
+
+            const filelist = await axios(commitTreeURL)
+            setFiles(filelist.data.tree.map((e: Tree) => e.path))
+        }
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+            {files.map((e) => (
+                <li key={e}>{e}</li>
+            ))}
+        </div>
+    )
 }
 
 export default App
