f57e80c15a8dd99235e3c67f0cbe92c58bc70b26
guotin <eero.kauhanen@helsinki.fi>
2020-09-16 09:40:19 +0300

    Add recursive search of files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,50 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 import ListView from './components/ListView'
 
-interface Tree {
-    path: string
+interface TreeNode {
+    path: string,
+    type: string,
+    url: string
 }
+
 
 const App = () => {
     const [files, setFiles] = useState<string[]>([])
 
     useEffect(() => {
         const fetchData = async () => {
-            const commits = await axios(
+            const commits: AxiosResponse<any> = await axios(
                 'https://api.github.com/repos/ohtuprojekti-eficode/robot-test-files/commits'
             )
+            const latestCommitTreeURL: string = commits.data[0].commit.tree.url
+            const rootTreePromise: AxiosResponse<any> = await axios(latestCommitTreeURL)
+            const rootTreeURL: string = rootTreePromise.data.url
 
-            const commitTreeURL: string = commits.data[0].commit.tree.url
+            const getFiles = async (newUrl: string) => {
+                const result: AxiosResponse<any> = await axios(newUrl)
+                const tree: TreeNode[] = result.data.tree
+                let filesToAdd: string[] = []
+                let i: number;
+                for (i = 0; i < tree.length; i++) {
+                    if (tree[i].type === "blob") {
+                        filesToAdd.push(tree[i].path)
+                    } else if (tree[i].type === "tree") {
+                        filesToAdd = filesToAdd.concat(await getFiles(tree[i].url))
+                    }
+                }
+                return filesToAdd
+            }
 
-            const filelist = await axios(commitTreeURL)
-            setFiles(filelist.data.tree.map((e: Tree) => e.path))
+            const fullList: string[] = await getFiles(rootTreeURL)
+            setFiles(fullList)
         }
         fetchData()
     }, [])
 
     return (
-        <div>
-            {files.map((e) => (
-                <li key={e}>{e}</li>
-            ))}
-        </div>
+        <ListView files={files} />
     )
 }
 
--- frontend/src/components/ListView.tsx
+++ frontend/src/components/ListView.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const ListView = () => {
-    return <div>ListView</div>
+
+const ListView = ({ files }: any) => {
+
+
+    return <div>
+        <h1>List of files in the repository</h1>
+        {files.map((e: string) => (
+            <li key={e}>{e}</li>
+        ))}
+    </div>
+
+
 }
 
 export default ListView
