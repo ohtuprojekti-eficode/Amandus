d5fcf5a5cec80ff32a1de871dbd704a74918c2e5
Ilkka <ijmakine@gmail.com>
2020-09-21 13:50:05 +0300

    file listing aside code editor

--- frontend/src/components/EditView.tsx
+++ frontend/src/components/EditView.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MonacoEditor from './MonacoEditor'
+import ListView from './ListView'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 
@@ -13,11 +14,29 @@ const EditView = ({ location }: any) => {
     const files = useSelector<RootState, RepoFile[]>(state => state.files.fileList)
     const content = files.find(e => e.filename === filename)?.content
     return (
-        <div>
-            <h1>{filename}</h1>
-            <MonacoEditor content={content} />
+        <div style={gridContainerStyle}>
+            <div style={asideContainerStyle}>
+                <ListView />
+            </div>
+            <div style={mainContainerStyle}>
+                <h2>{filename}</h2>
+                <MonacoEditor content={content} />
+            </div>
         </div>
     )
+}
+
+const gridContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between'
+}
+
+const asideContainerStyle = {
+    width: '20%'
+}
+
+const mainContainerStyle = {
+    width: '80%'
 }
 
 export default EditView
