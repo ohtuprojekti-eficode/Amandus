9bac7b81b8c57965e9d2be2a0bbd480dac70c052
ilkkamaksy <ijmakine@gmail.com>
2020-10-07 15:17:16 +0300

    changed gh callback url

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -9,7 +9,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'changeme'
 
 const GITHUB_CLIENT_ID = process.env.GH_CLIENT_ID
 const GITHUB_CLIENT_SECRET = process.env.GH_CLIENT_SECRET
-const GITHUB_CB_URL = 'http://localhost:3000/auth/github/callback'
+const GITHUB_CB_URL = process.env.GH_CB_URL || 'http://localhost:3000/auth/github/callback'
 
 export default {
   PORT,
