e9a6dc658b17eef6d89a8795eeb859b93425ad20
guotin <eero.kauhanen@helsinki.fi>
2020-10-07 16:11:26 +0300

    Make express host production frontend & github actions push whole application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,13 @@ import schema from './schema/schema'
 
 import { Req } from '../types/request'
 import User from './model/user'
+import path from 'path'
 
 const app = express()
 
 const corsOptions = {
   origin: true,
-  credentials: true
+  credentials: true,
 }
 
 interface TokenType {
@@ -24,21 +25,31 @@ const server = new ApolloServer({
   schema: schema,
   context: ({ req }: Req) => {
     const auth = req.headers.authorization
-    if ( auth && auth.toLowerCase().startsWith('bearer') ) {
+    if (auth && auth.toLowerCase().startsWith('bearer')) {
       const decodedToken = verify(auth.substring(7), config.JWT_SECRET)
-			const currentUser = User.getUserByGithubId((<TokenType>decodedToken).gitHubId)
-			return { currentUser }
+      const currentUser = User.getUserByGithubId(
+        (<TokenType>decodedToken).gitHubId
+      )
+      return { currentUser }
     }
     return null
-  }
+  },
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
 server.applyMiddleware({ app, cors: corsOptions })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'index.html'))
+  })
+}
+
 const httpServer = createServer(app)
 
-httpServer.listen(
-  { port: config.PORT },
-  (): void => console.log(`GraphQL is now running on http://localhost:${config.PORT}/graphql`)
+httpServer.listen({ port: config.PORT }, (): void =>
+  console.log(
+    `GraphQL is now running on http://localhost:${config.PORT}/graphql`
+  )
 )
