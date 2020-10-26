50c4e13cdf73e06156236bd0c28e1bf1c3a882ca
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-10-26 11:51:09 +0200

    Reformat MonacoEditor.tsx with Prettier

--- frontend/src/components/MonacoEditor.tsx
+++ frontend/src/components/MonacoEditor.tsx
@@ -7,72 +7,74 @@ import { ME } from '../graphql/queries'
 import { SAVE_CHANGES } from '../graphql/mutations'
 
 interface Props {
-  content: string | undefined,
+  content: string | undefined
   filename: string | undefined
 }
 
 interface Getter {
-    (): string
+  (): string
 }
 
 const MonacoEditor = ({ content, filename }: Props) => {
+  const {
+    loading: userQueryLoading,
+    error: userQueryError,
+    data: user,
+  } = useQuery(ME)
 
-    const { loading: userQueryLoading, error: userQueryError, data: user } = useQuery(ME)
-    
-    const [
-        saveChanges, 
-        { loading: mutationSaveLoading }
-    ] = useMutation(SAVE_CHANGES)
+  const [saveChanges, { loading: mutationSaveLoading }] = useMutation(
+    SAVE_CHANGES
+  )
 
-    const valueGetter = useRef<Getter | null>(null)
+  const valueGetter = useRef<Getter | null>(null)
 
-    const handleEditorDidMount = (_valueGetter: Getter) => {
-        valueGetter.current = _valueGetter
+  const handleEditorDidMount = (_valueGetter: Getter) => {
+    valueGetter.current = _valueGetter
+  }
+
+  const handleSaveButton = () => {
+    if (valueGetter.current) {
+      saveChanges({
+        variables: {
+          file: {
+            name: filename,
+            content: valueGetter.current(),
+          },
+          username: user.me.username,
+          email: user.me.gitHubEmail,
+          token: user.me.gitHubToken,
+          branch: 'master',
+        },
+      })
     }
+  }
 
-    const handleSaveButton = () => {
-        if (valueGetter.current) {
-            saveChanges({ variables: { 
-                    file: {
-                        name: filename,
-                        content: valueGetter.current(),
-                    },  
-                    username: user.me.username,
-                    email: user.me.gitHubEmail,
-                    token: user.me.gitHubToken,
-                    branch: 'master'
-                }
-            });
-        }
-    }
-
-    return (
-        <div style={{ border: '2px solid black', padding: '5px' }}>
-            <Editor
-                height="50vh"
-                language="javascript"
-                value={content}
-                editorDidMount={handleEditorDidMount}
-            />
-            <div>
-                <button
-                    disabled={
-                        userQueryLoading ||
-                        !!userQueryError ||
-                        mutationSaveLoading ||
-                        !user.me
-                    }
-                    onClick={handleSaveButton}
-                    >Save
-                </button>
-            </div>
-            <div style={{ fontSize: 14, marginTop: 5, marginBottom: 5 }}>
-                {(!user || !user.me) && 
-                        "Please login to enable saving"}
-            </div>
-            
-        </div>
-    )
+  return (
+    <div style={{ border: '2px solid black', padding: '5px' }}>
+      <Editor
+        height="50vh"
+        language="javascript"
+        value={content}
+        editorDidMount={handleEditorDidMount}
+      />
+      <div>
+        <button
+          disabled={
+            userQueryLoading ||
+            !!userQueryError ||
+            mutationSaveLoading ||
+            !user.me
+          }
+          onClick={handleSaveButton}
+        >
+          Save
+        </button>
+      </div>
+      <div style={{ fontSize: 14, marginTop: 5, marginBottom: 5 }}>
+        {(!user || !user.me) && 'Please login to enable saving'}
+      </div>
+    </div>
+  )
 }
 
 export default MonacoEditor
