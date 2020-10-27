809fabbcdc316bb5848ab52d47848f4317053989
guotin <eero.kauhanen@helsinki.fi>
2020-10-27 17:09:36 +0200

    Remove frontend duplicate loading and improve layout

--- frontend/src/components/ListView.tsx
+++ frontend/src/components/ListView.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FileList } from '../types'
+import { File } from '../types'
 
-const ListView = ({ files }: FileList) => {
+interface PropsType {
+  files: File[]
+}
+
+const ListView = ({ files }: PropsType) => {
   return (
     <div>
       <h2>Files in the repository</h2>
