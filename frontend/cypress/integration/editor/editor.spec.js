8ac1b6115eb18a714349deaf7a0811709b1d4b68
guotin <eero.kauhanen@helsinki.fi>
2020-10-29 08:55:21 +0200

    Change cypress test functions to arrow functions

--- frontend/cypress/integration/editor/editor.spec.js
+++ frontend/cypress/integration/editor/editor.spec.js
@@ -1,29 +1,29 @@
-describe('When visiting the application, as a user', function () {
-  beforeEach(function () {
+describe('When visiting the application, as a user', () => {
+  beforeEach(() => {
     cy.visit(Cypress.env('HOST'))
   })
 
-  it('I can move to edit view page', function () {
+  it('I can move to edit view page', () => {
     cy.contains('Edit view').click()
     cy.url().should('include', '/edit')
   })
 })
 
-describe('When visiting the edit view page, as a user', function () {
-  beforeEach(function () {
+describe('When visiting the edit view page, as a user', () => {
+  beforeEach(() => {
     cy.visit(Cypress.env('HOST') + '/edit')
   })
 
-  it('I can view the file listing', function () {
+  it('I can view the file listing', () => {
     cy.contains('Files in the repository')
   })
 
-  it('I can open a file in editor', function () {
+  it('I can open a file in editor', () => {
     cy.contains('example.txt').click()
     cy.contains('ohtuprojekti-eficode/robot-test-files/example.txt')
   })
 
-  it('I can edit a file in editor and see a save button', function () {
+  it('I can edit a file in editor and see a save button', () => {
     cy.contains('example.txt').click()
     cy.contains('Save')
   })
--- frontend/cypress/integration/editor/login.spec.js
+++ frontend/cypress/integration/editor/login.spec.js
@@ -1,20 +1,20 @@
-describe('When visiting the application, as a user', function () {
-  beforeEach(function () {
+describe('When visiting the application, as a user', () => {
+  beforeEach(() => {
     cy.visit(Cypress.env('HOST'))
   })
 
-  it('I can move to login page', function () {
+  it('I can move to login page', () => {
     cy.contains('Login').click()
     cy.url().should('include', '/login')
   })
 })
 
-describe('When visiting the login page, as a user', function () {
-  beforeEach(function () {
+describe('When visiting the login page, as a user', () => {
+  beforeEach(() => {
     cy.visit(Cypress.env('HOST') + '/login')
   })
 
-  it('I can see the github login button', function () {
+  it('I can see the github login button', () => {
     cy.url().should('include', '/login')
     cy.contains('Login with GitHub')
   })
