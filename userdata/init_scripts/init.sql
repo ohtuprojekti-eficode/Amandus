2608c926737fad606136fcaac48d6c851d565230
Auli Tallberg <auli.tallberg@gmail.com>
2020-10-31 14:54:34 +0200

    database contains 4 tables with relations

--- userdata/init_scripts/init.sql
+++ userdata/init_scripts/init.sql
@@ -1,8 +1,29 @@
-CREATE TABLE accounts(
-    account_id serial PRIMARY KEY,
-    webaddress VARCHAR,
-    account VARCHAR UNIQUE NOT NULL,
-    serviceName VARCHAR,
+CREATE TABLE USERS(
+    user_id serial PRIMARY KEY,    
+    username VARCHAR UNIQUE NOT NULL,
+    email VARCHAR,
     created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP
 );
+
+CREATE TABLE SERVICES(
+    service_id serial PRIMARY KEY,
+    name VARCHAR
+);
+
+CREATE TABLE SERVICE_USERS(
+    service_user_id serial PRIMARY KEY,
+    user_id INTEGER REFERENCES USERS (user_id),
+    service_id INTEGER REFERENCES SERVICES (service_id),
+    username VARCHAR, 
+    email VARCHAR,
+    token VARCHAR,
+    reposurl VARCHAR
+);
+
+CREATE TABLE REPO(
+    repo_id serial PRIMARY KEY,
+    service_user_id INTEGER REFERENCES SERVICE_USERS (service_user_id),
+    web_url VARCHAR
+);
+
