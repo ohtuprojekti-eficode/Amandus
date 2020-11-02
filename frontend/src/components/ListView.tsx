efec45a1fc2328c1e771628793de973b20cf6f1a
Simon Nyman <nyman.simon@hotmail.com>
2020-11-02 23:09:29 +0200

    Added branch selector with WIP-branches

--- frontend/src/components/ListView.tsx
+++ frontend/src/components/ListView.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { File } from '../types'
+import BranchSelector from './BranchSelector'
 
 interface PropsType {
   files: File[]
@@ -9,14 +10,26 @@ interface PropsType {
 const ListView = ({ files }: PropsType) => {
   return (
     <div>
-      <h2>Files in the repository</h2>
-      <ul>
-        {files.map((e) => (
-          <li key={e.name}>
-            <Link to={`edit?q=${e.name}`}>{e.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="branch-selector">
+        <BranchSelector
+          branches={[
+            'master',
+            'staging',
+            'feature-branch-3',
+            'feature-branch-whose-name-is-wayy-too-long',
+          ]}
+        />
+      </div>
+      <div className="file-listing">
+        <h2>Files in the repository</h2>
+        <ul>
+          {files.map((e) => (
+            <li key={e.name}>
+              <Link to={`edit?q=${e.name}`}>{e.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
