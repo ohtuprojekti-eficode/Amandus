99dc9a189feabd1a8dc4e6f002a218de8d8a7e0f
tulma95 <riku.honkanen@helsinki.fi>
2020-11-02 12:51:00 +0200

    Add tests for getBranches function

---
+++ backend/src/tests/git.test.ts
@@ -0,0 +1,40 @@
+import simpleGit from 'simple-git'
+import { mkdirSync, rmdirSync, appendFileSync } from 'fs'
+import { join } from 'path'
+import { getBranches } from '../services/git'
+
+describe('Get branches', () => {
+  const repoPath = join(__dirname, 'testRepo')
+  beforeEach(() => {
+    mkdirSync(repoPath)
+  })
+
+  afterEach(() => {
+    rmdirSync(repoPath, { recursive: true })
+  })
+
+  it('returns empty list if no branches on repo', async () => {
+    const testRepo = simpleGit(repoPath)
+    await testRepo.init()
+
+    const branches = await getBranches(testRepo)
+    expect(branches).toEqual([])
+  })
+
+  it('returns added local branches', async () => {
+    appendFileSync(
+      `${repoPath}/file.txt`,
+      'Commit and add file to create master branch'
+    )
+
+    const testRepo = simpleGit(repoPath)
+    await testRepo
+      .init()
+      .add('.')
+      .commit('init commit')
+      .branch(['secondBranch'])
+
+    const branches = await getBranches(testRepo)
+    expect(branches).toEqual(['master', 'secondBranch'])
+  })
+})
