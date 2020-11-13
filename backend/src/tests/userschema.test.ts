90ddbcaae6273c14594f27e48656f47c4bd3176c
guotin <eero.kauhanen@helsinki.fi>
2020-11-13 10:53:06 +0200

    Fix tests in github actions

--- backend/src/tests/userschema.test.ts
+++ backend/src/tests/userschema.test.ts
@@ -1,5 +1,6 @@
 import { createTestClient } from 'apollo-server-testing'
 import gql from 'graphql-tag'
+import { closePool } from '../db/connect'
 import { server } from '../index'
 import User from '../model/user'
 
@@ -39,7 +40,7 @@ const ME = gql`
 const REGISTER = gql`
   mutation register($username: String!, $email: String!, $password: String!) {
     register(username: $username, email: $email, password: $password) {
-      token,
+      token
       user {
         id
         username
@@ -51,7 +52,7 @@ const REGISTER = gql`
 const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
-      token,
+      token
       user {
         id
         username
@@ -61,15 +62,11 @@ const LOGIN = gql`
 `
 
 describe('User schema register mutations', () => {
-
   beforeEach(async () => {
-    
     await User.deleteAll()
-
   })
-  
+
   it('user can register with valid username, email and password', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
@@ -77,16 +74,15 @@ describe('User schema register mutations', () => {
       variables: {
         username: 'testuser2',
         email: 'testuser2@test.com',
-        password: 'mypassword'
-      }
+        password: 'mypassword',
+      },
     })
-    
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(res.data?.register?.user?.username).toEqual('testuser2')
   })
 
   it('backend returns a token after user has successfully registered', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
@@ -94,16 +90,15 @@ describe('User schema register mutations', () => {
       variables: {
         username: 'testuser2',
         email: 'testuser2@test.com',
-        password: 'mypassword'
-      }
+        password: 'mypassword',
+      },
     })
-    
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(res.data?.register?.token).toBeTruthy()
   })
 
   it('user can not register without a username', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
@@ -111,18 +106,20 @@ describe('User schema register mutations', () => {
       variables: {
         username: '',
         email: 'testuser2@test.com',
-        password: 'mypassword'
-      }
+        password: 'mypassword',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Username, email or password can not be empty')
+
+    const errorFound = res.errors?.some(
+      (error) =>
+        error.message === 'Username, email or password can not be empty'
+    )
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
 
   it('user can not register without a password', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
@@ -130,18 +127,20 @@ describe('User schema register mutations', () => {
       variables: {
         username: 'testuser2',
         email: 'testuser2@test.com',
-        password: ''
-      }
+        password: '',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Username, email or password can not be empty')
+
+    const errorFound = res.errors?.some(
+      (error) =>
+        error.message === 'Username, email or password can not be empty'
+    )
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
 
   it('user can not register without an email address', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
@@ -149,97 +148,98 @@ describe('User schema register mutations', () => {
       variables: {
         username: 'testuser2',
         email: '',
-        password: 'mypassword'
-      }
+        password: 'mypassword',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Username, email or password can not be empty')
+
+    const errorFound = res.errors?.some(
+      (error) =>
+        error.message === 'Username, email or password can not be empty'
+    )
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
-
 })
 
 describe('User schema login mutations', () => {
-
   beforeEach(async () => {
-    
     await User.deleteAll()
-  
-		const userData = {
-      username: 'testuser',
-			email: 'testuser@test.com',
-			password: 'testpassword'
-		}
-        
-		await User.registerUser(userData)
 
+    const userData = {
+      username: 'testuser',
+      email: 'testuser@test.com',
+      password: 'testpassword',
+    }
+
+    await User.registerUser(userData)
   })
-  
+
   it('user can login with valid username and password', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
       mutation: LOGIN,
       variables: {
         username: 'testuser',
-        password: 'testpassword'
-      }
+        password: 'testpassword',
+      },
     })
-    
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(res.data?.login?.user?.username).toEqual('testuser')
   })
 
   it('user can not login without username and password', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
       mutation: LOGIN,
       variables: {
         username: '',
-        password: ''
-      }
+        password: '',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Invalid username or password')
+
+    const errorFound = res.errors?.some(
+      (error) => error.message === 'Invalid username or password'
+    )
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
 
   it('user can not login without a valid password', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
       mutation: LOGIN,
       variables: {
         username: 'testuser',
-        password: 'wrongpass'
-      }
+        password: 'wrongpass',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Invalid username or password')
+
+    const errorFound = res.errors?.some(
+      (error) => error.message === 'Invalid username or password'
+    )
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
 
   it('user can not login without a valid username', async () => {
-    
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
       mutation: LOGIN,
       variables: {
         username: 'wronguser',
-        password: 'testpassword'
-      }
+        password: 'testpassword',
+      },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Invalid username or password')
+
+    const errorFound = res.errors?.some(
+      (error) => error.message === 'Invalid username or password'
+    )
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     expect(errorFound).toBeTruthy()
   })
@@ -249,41 +249,44 @@ describe('User schema login mutations', () => {
 
     const res = await mutate({
       mutation: AUTHORIZE_WITH_GH,
-      variables: { code: 'invalid' }
+      variables: { code: 'invalid' },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Invalid or expired GitHub code')
+
+    const errorFound = res.errors?.some(
+      (error) => error.message === 'Invalid or expired GitHub code'
+    )
     expect(errorFound).toBeTruthy()
   })
 })
 
-
 describe('User schema GitHub auth mutations', () => {
-  
   it('user can not authorize with an invalid GitHub code', async () => {
     const { mutate } = createTestClient(server)
 
     const res = await mutate({
       mutation: AUTHORIZE_WITH_GH,
-      variables: { code: 'invalid' }
+      variables: { code: 'invalid' },
     })
-    
-    const errorFound = res.errors?.some(error => error.message === 'Invalid or expired GitHub code')
+
+    const errorFound = res.errors?.some(
+      (error) => error.message === 'Invalid or expired GitHub code'
+    )
     expect(errorFound).toBeTruthy()
   })
 })
 
-
 describe('User schema logged out queries', () => {
-  
   it('no user data is returned when user is not logged in', async () => {
     const { query } = createTestClient(server)
 
     const res = await query({
-      query: ME
+      query: ME,
     })
-    
+
     expect(res.data?.me).toBeNull()
   })
+})
 
+afterAll(async () => {
+  await closePool()
 })
