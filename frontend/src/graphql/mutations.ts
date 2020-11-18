f8b017e18381a376934149e22d4c67fa79e1934c
Simon Nyman <nyman.simon@hotmail.com>
2020-11-18 17:11:50 +0200

    Added front end GQL mutation

--- frontend/src/graphql/mutations.ts
+++ frontend/src/graphql/mutations.ts
@@ -38,4 +38,14 @@ export const REGISTER = gql`
   }
 `
 
-export const LOGIN = gql``
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        id
+        username
+      }
+    }
+  }
+`
