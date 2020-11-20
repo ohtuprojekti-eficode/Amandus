735645bc0ba5a5e089732db7553262b439d71cec
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-11-20 16:11:23 +0200

    Remove obsolete cypress test

--- frontend/cypress/integration/editor/login.spec.js
+++ frontend/cypress/integration/editor/login.spec.js
@@ -8,14 +8,3 @@ describe('When visiting the application, as a user', () => {
     cy.url().should('include', '/login')
   })
 })
-
-describe('When visiting the login page, as a user', () => {
-  beforeEach(() => {
-    cy.visit(Cypress.env('HOST') + '/login')
-  })
-
-  it('I can see the github login button', () => {
-    cy.url().should('include', '/login')
-    cy.contains('Login with GitHub')
-  })
-})
