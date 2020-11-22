faa84caaea8e9ce7cee973f153f74f686ef82047
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-11-23 00:01:30 +0200

    Add ability to toggle between dark and light themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { ME } from './graphql/queries'
@@ -6,11 +6,28 @@ import EditView from './components/EditView'
 import Header from './components/Header'
 import LoginForm from './components/LoginForm'
 import CallBack from './components/auth/CallBack'
-import { Toolbar } from '@material-ui/core'
 import RegisterForm from './components/RegisterForm'
+import { CssBaseline, Toolbar } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
 const App = () => {
   const { data: user } = useQuery(ME)
+
+  const defaultColorTheme = 'light'
+  const [colorTheme, setColorTheme] = useState<'light' | 'dark'>(
+    defaultColorTheme
+  )
+
+  const toggleColorTheme = () => {
+    const newColorTheme = colorTheme === 'light' ? 'dark' : 'light'
+    setColorTheme(newColorTheme)
+  }
+
+  const theme = createMuiTheme({
+    palette: {
+      type: colorTheme,
+    },
+  })
 
   const logout = () => {
     localStorage.clear()
@@ -19,18 +36,26 @@ const App = () => {
 
   return (
     <div>
-      <Header user={user?.me} logout={logout} />
-      <div>
-        <Toolbar />
-        <Route path="/auth/github/callback">
-          <CallBack />
-        </Route>
-        <Route path="/edit">
-          <EditView />
-        </Route>
-        <Route exact path="/login" component={LoginForm} />
-        <Route exact path="/register" component={RegisterForm} />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header
+          user={user?.me}
+          logout={logout}
+          colorTheme={colorTheme}
+          toggleColorTheme={toggleColorTheme}
+        />
+        <div>
+          <Toolbar />
+          <Route path="/auth/github/callback">
+            <CallBack />
+          </Route>
+          <Route path="/edit">
+            <EditView />
+          </Route>
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/register" component={RegisterForm} />
+        </div>
+      </ThemeProvider>
     </div>
   )
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { AppBar, Toolbar } from '@material-ui/core'
+import React, { useState } from 'react'
+import { AppBar, Switch, Toolbar } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { UserType } from '../types'
 
 interface Props {
   user: UserType
   logout: () => void
+  colorTheme: string
+  toggleColorTheme: () => void
 }
 
 const padding = {
   paddingRight: 5,
 }
 
-const Header = ({ user, logout }: Props) => {
+const Header = ({ user, logout, colorTheme, toggleColorTheme }: Props) => {
+  const [switchChecked, setSwitchChecked] = useState(colorTheme === 'dark')
+
+  const handleSwitchToggle = () => {
+    setSwitchChecked(!switchChecked)
+    toggleColorTheme()
+  }
+
   return (
     <AppBar position="fixed" color="default" style={{ zIndex: 1250 }}>
       <Toolbar>
@@ -37,6 +46,12 @@ const Header = ({ user, logout }: Props) => {
             {user.username} - logout
           </Link>
         )}
+        <Switch
+          checked={switchChecked}
+          onChange={handleSwitchToggle}
+          color="primary"
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
       </Toolbar>
     </AppBar>
   )
