00528a2b638fff654dc881524eae23a9922032d7
ilkkamaksy <ijmakine@gmail.com>
2020-11-24 19:34:17 +0200

    added tests for failed login

--- frontend/cypress/integration/editor/login.spec.js
+++ frontend/cypress/integration/editor/login.spec.js
@@ -6,6 +6,7 @@ describe('When visiting the application, as a user', () => {
   it('I can move to login page', () => {
     cy.contains('Login').click()
     cy.url().should('include', '/login')
+    cy.contains('Login')
   })
 })
 
@@ -14,8 +15,25 @@ describe('When visiting the login page, as a user', () => {
     cy.visit(Cypress.env('HOST') + '/login')
   })
 
-  it('I can see the login text', () => {
-    cy.url().should('include', '/login')
-    cy.contains('Login')
+  it('I see an error message when I try to log in with invalid credentials', () => {
+    cy.get('#username').type('testuser')
+    cy.get('#password').type('TestPassword!111')
+    cy.get('form button').click()
+    cy.contains('Login failed. Please try again.')
   })
+
+  it('I see a red message under the password field when I try to log in without a password', () => {
+    cy.get('#username').type('testuser')
+    cy.get('form button').click()
+    cy.get('#password-helper-text').should('have.css', 'color', 'rgb(176, 0, 32)')
+    cy.contains('Please enter your password.')
+  })
+
+  it('I see a red message under the username field when I try to log in without a username', () => {
+    cy.get('#password').type('testuser')
+    cy.get('form button').click()
+    cy.get('#username-helper-text').should('have.css', 'color', 'rgb(176, 0, 32)')
+    cy.contains('Please enter your username.')
+  })
+  
 })
