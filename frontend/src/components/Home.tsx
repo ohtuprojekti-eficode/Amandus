db79df5527ef116fd34c86a31f90115f57fa9844
ilkkamaksy <ijmakine@gmail.com>
2020-12-05 09:51:14 +0200

    background styles and register button on hero

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,53 +1,46 @@
 import React from 'react'
 import {
-  Grid,
-  TextField,
+  Link,
   Button,
   makeStyles,
   createStyles,
 } from '@material-ui/core'
-
+import { Link as RouterLink } from 'react-router-dom'
 
 const stylesInUse = makeStyles((theme) =>
   createStyles({
     root: {
-      backgroundColor: '#6200EE',
-      backgroundImage: `url(${process.env.PUBLIC_URL}/img/cool-background.png)`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
+      background: `linear-gradient(135deg, hsla(307.46, 94.72%, 46.32%, 1) 0%, hsla(307.46, 94.72%, 46.32%, 0) 70%),
+          linear-gradient(25deg, hsla(274.67, 92.84%, 45.16%, 1) 10%, hsla(274.67, 92.84%, 45.16%, 0) 80%),
+          linear-gradient(315deg, hsla(256.77, 98.88%, 40.21%, 1) 15%, hsla(256.77, 98.88%, 40.21%, 0) 80%),
+          linear-gradient(245deg, hsla(95.73, 93.7%, 43.15%, 1) 100%, hsla(95.73, 93.7%, 43.15%, 0) 70%)`,
       display: 'flex',
       position: 'relative',
       justifyContent: 'center',
       alignItems: 'center',
       height: 'calc(100vh - 64px)',
-      '&::after': {
-          content: '""',
-          display: 'block',
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          background: `linear-gradient(135deg, hsla(307.46, 94.72%, 46.32%, 1) 0%, hsla(307.46, 94.72%, 46.32%, 0) 70%),
-          linear-gradient(25deg, hsla(274.67, 92.84%, 45.16%, 1) 10%, hsla(274.67, 92.84%, 45.16%, 0) 80%),
-          linear-gradient(315deg, hsla(256.77, 98.88%, 40.21%, 1) 15%, hsla(256.77, 98.88%, 40.21%, 0) 80%),
-          linear-gradient(245deg, hsla(95.73, 93.7%, 43.15%, 1) 100%, hsla(95.73, 93.7%, 43.15%, 0) 70%)`
-      }
     },
     intro: {
       color: '#ffffff',
       textAlign: 'center',
       position: 'relative',
-      zIndex: 1
+      zIndex: 1,
     },
     heading: {
-      fontSize: '7em',
+      fontSize: '5rem',
       position: 'relative',
       fontWeight: 'bold',
       letterSpacing: -2,
       marginBottom: 0
     },
     introText: {
-      fontSize: '1.6em',
+      fontSize: '1.6rem',
       marginTop: 0
+    },
+    colorBtn: {
+      backgroundColor: '#df0cc4',
+      padding: '12px 45px',
+      fontWeight: 'bold',
     }
   })
 )
@@ -61,6 +54,16 @@ const Home = () => {
           <div className={classes.intro}>
             <h1 className={classes.heading}>Test automation made easy</h1>
             <p className={classes.introText}>Amandus is an open source web based IDE with integrated version control and support for Robot Framework.</p>
+            <Button
+              color="primary"
+              className={classes.colorBtn}
+              variant="contained"
+              href="/register"
+              size="large"
+              disableElevation
+            >
+                Register
+            </Button>
           </div>
         </div>
     )
