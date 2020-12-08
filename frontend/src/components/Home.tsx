35d21d95d29cfd9eedb7294b6819134df27a715e
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2020-12-08 08:56:02 +0200

    Use history.push() in home view buttons

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import {
-  Button,
-  makeStyles,
-  createStyles,
-} from '@material-ui/core'
+import { Button, makeStyles, createStyles } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
-const stylesInUse = makeStyles((theme) =>
+const stylesInUse = makeStyles(() =>
   createStyles({
     root: {
       background: `linear-gradient(135deg, hsla(307.46, 94.72%, 46.32%, 1) 0%, hsla(307.46, 94.72%, 46.32%, 0) 70%),
@@ -29,18 +26,18 @@ const stylesInUse = makeStyles((theme) =>
       position: 'relative',
       fontWeight: 'bold',
       letterSpacing: -2,
-      marginBottom: 0
+      marginBottom: 0,
     },
     introText: {
       fontSize: '1.6rem',
-      marginTop: 0
+      marginTop: 0,
     },
     containedBtn: {
       backgroundColor: '#df0cc4',
       padding: '12px 0',
       fontWeight: 'bold',
       margin: '0 0.5em',
-      width: 180
+      width: 180,
     },
     outlinedBtn: {
       padding: '12px 0',
@@ -50,46 +47,54 @@ const stylesInUse = makeStyles((theme) =>
       width: 180,
       borderColor: 'rgba(255,255,255,0.5)',
       '&:hover': {
-        borderColor: '#fff'
-      }
+        borderColor: '#fff',
+      },
     },
   })
 )
 
 const Home = () => {
+  const classes = stylesInUse()
+  const history = useHistory()
 
-    const classes = stylesInUse()
+  const handleClick = (path: string) => {
+    return () => {
+      history.push(path)
+    }
+  }
 
-    return (
-        <div className={classes.root}>
-          <div className={classes.intro}>
-            <h1 className={classes.heading}>Test automation made easy</h1>
-            <p className={classes.introText}>Amandus is an open source web based IDE with integrated version control and support for Robot Framework.</p>
-      
-              <Button
-                color="primary"
-                className={classes.containedBtn}
-                variant="contained"
-                href="/register"
-                size="large"
-                disableElevation
-              >
-                  Register   
-              </Button>
-              <Button
-                color="secondary"
-                className={classes.outlinedBtn}
-                variant="outlined"
-                href="/login"
-                size="large"
-                disableElevation
-              >
-                  Login
-              </Button>
-      
-          </div>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <div className={classes.intro}>
+        <h1 className={classes.heading}>Test automation made easy</h1>
+        <p className={classes.introText}>
+          Amandus is an open source web based IDE with integrated version
+          control and support for Robot Framework.
+        </p>
+
+        <Button
+          color="primary"
+          className={classes.containedBtn}
+          variant="contained"
+          size="large"
+          onClick={handleClick('/register')}
+          disableElevation
+        >
+          Register
+        </Button>
+        <Button
+          color="secondary"
+          className={classes.outlinedBtn}
+          variant="outlined"
+          size="large"
+          onClick={handleClick('/login')}
+          disableElevation
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Home
