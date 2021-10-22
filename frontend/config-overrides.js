37361b22bd8f294a0320159e2c9dad06eae47fc3
gitblast <jarkko.mammela@gmail.com>
2021-10-22 12:40:33 +0300

    fix rewire breaking jest config

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,12 +1,25 @@
-module.exports = function override(config, env) {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.resolve.alias,
-      vscode: require.resolve(
+module.exports = {
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        vscode: require.resolve(
+          '@codingame/monaco-languageclient/lib/vscode-compatibility'
+        ),
+      },
+    }
+    return config
+  },
+  jest: (config) => {
+    config.moduleNameMapper = {
+      '^vscode$': require.resolve(
         '@codingame/monaco-languageclient/lib/vscode-compatibility'
       ),
-    },
-  }
-  return config
+    }
+
+    config.transformIgnorePatterns = ['node_modules/(?!@monaco-editor)/']
+
+    return config
+  },
 }
