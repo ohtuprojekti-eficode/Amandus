cb6a9d1aebeef74b64415a1bfc0159745cd147f0
ikylios <ikylioss@gmail.com>
2021-10-25 18:22:05 +0300

    plugins now shown as list of pluginObjects

--- frontend/src/components/AdminPage.tsx
+++ frontend/src/components/AdminPage.tsx
@@ -7,16 +7,58 @@ interface Props {
   user: UserType | undefined
 }
 
-const AdminPage = ({ user }: Props) => {
+interface pluginObject { 
+  name: string; 
+  data: string; 
+}
+
+const PluginObject = ({ name, data }: pluginObject ) => {
 
   const [switchChecked, setSwitchChecked] = useState(false)
 
   const handleSwitchToggle = () => {
     setSwitchChecked(!switchChecked)
   }
+  
+  return (
+    <div>
+      <b>{name}</b>
+      <Switch
+        id="robot-language-server-toggle"
+        name="robot-language-server-toggle"
+        checked={switchChecked}
+        onChange={handleSwitchToggle}
+        color="primary"
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      {switchChecked ? 'on' : 'off'}
+    </div>
+  )
+}
 
-// Hides view from users that are not admins
+const pluginList = () => {
 
+  const plugins: pluginObject[] = [
+    { name: '1. plugin', data: 'data1' },
+    { name: '2. plugin', data: 'data2' },
+    { name: '3. plugin', data: 'data3' },
+  ]
+
+  //  console.log(plugins)
+
+  return (
+    <div>
+      {plugins.map(p => 
+        <PluginObject name={p.name} data={p.data} /> 
+      )}
+    </div>
+  ) 
+}
+
+const AdminPage = ({ user }: Props) => {
+
+// Hides view from users that are not admins. Uncomment after this page is fully implemented
+/*
   if (user?.user_role !== 'admin') {
     return (
       <h1>
@@ -24,24 +66,7 @@ const AdminPage = ({ user }: Props) => {
       </h1>
     )
   }
-
-
-  
-  const pluginList = () => {
-    return (
-      <div>
-        <Switch
-          id="robot-language-server-toggle"
-          name="robot-language-server-toggle"
-          checked={switchChecked}
-          onChange={handleSwitchToggle}
-          color="primary"
-          inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
-        {switchChecked ? 'checked' : 'not checked'}
-      </div>
-    ) 
-  }
+*/
 
   return (
     <div >
