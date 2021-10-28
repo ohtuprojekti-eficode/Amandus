e641adae36635e790b5f6bb135205d66b28a29ea
lauripalonen <palonen.lauri@gmail.com>
2021-10-28 16:24:21 +0300

    add necessary ts and eslint configs, make the skeleton walk

--- tokenservice/index.ts
+++ tokenservice/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello world')
+import express from 'express'
+const app = express()
+
+let tokens = [
+  {id: 1, token: 'a'},
+  {id: 2, token: 'b'},
+  {id: 3, token: 'c'},
+  {id: 4, token: 'd'}
+]
+
+app.get('/', (_req, res) => {
+  res.send('<H1>Token service<H1>')
+})
+
+app.get('/api/tokens', (_req, res) => {
+  res.json(tokens)
+})
+
+app.get('/api/tokens/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  const token = tokens.find(t => t.id === id)
+
+  if(token) {
+    res.json(token)
+  } else {
+    res.status(404).end
+  }
+})
+
+const PORT = 3002
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
