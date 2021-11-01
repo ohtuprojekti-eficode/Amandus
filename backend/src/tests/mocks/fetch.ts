c8d9f0c268c9f23393ba46372072575c867a76e0
Juuso Kotimäki <jkotima@gmail.com>
2021-11-01 13:51:07 +0200

    implement mock for fetch, unit tests for services

---
+++ backend/src/tests/mocks/fetch.ts
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { RequestInfo, RequestInit } from 'node-fetch'
+const { Response } = jest.requireActual('node-fetch')
+
+const toFetchResponse = (object: Record<string, unknown>) =>
+  Promise.resolve(new Response(JSON.stringify(object)))
+
+const nodeFetchMock =
+  jest.fn().mockImplementation((url: RequestInfo, init?: RequestInit | undefined) => {
+    void init
+
+    switch (url) {
+      case 'https://github.com/login/oauth/access_token':
+      case 'https://gitlab.com/oauth/token':
+      case 'https://bitbucket.org/site/oauth2/access_token':
+        return toFetchResponse({ access_token: "immatokenlol" })
+
+      case 'https://api.github.com/user':
+        return toFetchResponse(
+          {
+            login: 'elvis',
+            email: 'elvis@detroit.us',
+            repos_url: 'https://api.github.com/elvis/repos'
+          }
+        )
+
+      case 'https://gitlab.com/api/v4/user':
+        return toFetchResponse(
+          {
+            username: 'elvis',
+            email: 'elvis@detroit.us'
+          }
+        )
+
+      case 'https://api.bitbucket.org/2.0/user':
+        return toFetchResponse(
+          {
+            username: 'elvis',
+            links: {
+              repositories: {
+                href: 'https://api.bitbucket.org/2.0/repositories/elvis'
+              }
+            }
+          }
+        )
+
+      case 'https://api.bitbucket.org/2.0/user/emails':
+        return toFetchResponse({
+          values:
+            [
+              {
+                is_primary: true,
+                email: 'elvis@detroit.us'
+              }
+            ]
+        })
+
+      default:
+        throw new Error(`Url "${url.toString()}"" is not defined in mocked node_fetch`)
+
+    }
+
+  })
+
+module.exports = nodeFetchMock
---
+++ backend/src/tests/services.test.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import { requestBitbucketUser } from "../services/bitbucket"
+import { requestGithubUser } from "../services/gitHub"
+import { requestGitLabUser } from "../services/gitLab"
+
+// import fetch from 'node-fetch'
+jest.mock('node-fetch', () => require('./mocks/fetch'))
+
+describe('Bitbucket API', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('requestBitbucketUser returns correct access_token, username, reposurl and email from mock', async () => {
+    const bitbucketUser = await requestBitbucketUser('123code321')
+    expect(bitbucketUser.access_token).toBe('immatokenlol')
+    expect(bitbucketUser.serviceUser.username).toBe('elvis')
+    expect(bitbucketUser.serviceUser.reposurl).toBe('https://api.bitbucket.org/2.0/repositories/elvis')
+    expect(bitbucketUser.serviceUser.email).toBe('elvis@detroit.us')
+  })
+})
+
+describe('Gitlab API', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('requestGitlabUser returns correct access_token, username and email from mock', async () => {
+    const gitlabUser = await requestGitLabUser('123code321')
+    expect(gitlabUser.access_token).toBe('immatokenlol')
+    expect(gitlabUser.serviceUser.username).toBe('elvis')
+    expect(gitlabUser.serviceUser.email).toBe('elvis@detroit.us')
+  })
+
+  it('requestGitlabUser returns correct reposurl', async () => {
+    const gitlabUser = await requestGitLabUser('123code321')
+    expect(gitlabUser.serviceUser.reposurl).toBe('https://gitlab.com/api/v4/projects?simple=true&min_access_level=30')  
+  })
+})
+
+describe('Github API', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('requestGithubUser returns correct access_token, username, reposurl and email from mock', async () => {
+    const githubUser = await requestGithubUser('123code321')
+    expect(githubUser.access_token).toBe('immatokenlol')
+    expect(githubUser.serviceUser.username).toBe('elvis')
+    expect(githubUser.serviceUser.email).toBe('elvis@detroit.us')
+    expect(githubUser.serviceUser.reposurl).toBe('https://api.github.com/elvis/repos')
+  })
+})
