0ad214b67d07039f82591fe7cbc09f90de15b793
lauripalonen <palonen.lauri@gmail.com>
2021-11-02 16:08:05 +0200

    fetch port from config file

--- tokenservice/src/index.ts
+++ tokenservice/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import tokenRouter from './routes/tokenRouter'
 import cors from 'cors'
+import config from './utils/config'
 
 const app = express()
 
@@ -9,7 +10,7 @@ app.use(cors())
 
 app.use('/api/tokens', tokenRouter)
 
-const PORT = 3002
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
--- tokenservice/src/utils/config.ts
+++ tokenservice/src/utils/config.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const PORT = process.env.PORT || 3002
+
 const JWT_SECRET = process.env.JWT_SECRET || 'changeme'
 
 const GITLAB_CLIENT_ID = 'glcid'
@@ -13,6 +15,7 @@ const BITBUCKET_CLIENT_SECRET = 'bbcsec'
 const BITBUCKET_CB_URL = 'bb_cb_url'
 
 export default {
+  PORT,
   JWT_SECRET,
   GITLAB_CLIENT_ID,
   GITLAB_CLIENT_SECRET,
