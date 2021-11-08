1316272436a3eb83ffe51c823b259f4d06eab94b
Juuso Kotimäki <jkotima@gmail.com>
2021-11-08 13:58:59 +0200

    cypress tests for repositories-page

--- backend/src/tests/mocks/fetch.ts
+++ backend/src/tests/mocks/fetch.ts
@@ -1,6 +1,6 @@
 import { RequestInfo, RequestInit, Response } from 'node-fetch'
 
-const toFetchResponse = (object: Record<string, unknown>) =>
+const toFetchResponse = (object: Record<string, unknown> | Record<string, unknown>[]) =>
   Promise.resolve(new Response(JSON.stringify(object)))
 
 const nodeFetchMock = (url: RequestInfo, _init?: RequestInit | undefined): Promise<Response> => {
@@ -51,6 +51,82 @@ const nodeFetchMock = (url: RequestInfo, _init?: RequestInit | undefined): Promi
               email: 'elvis@detroit.us'
             }
           ]
+      })
+
+    case 'https://api.github.com/elvis/repos':
+      return toFetchResponse([
+        {
+          id: 123,
+          name: 'ghtestrepo1',
+          full_name: 'elvis/ghtestrepo1',
+          clone_url: 'https://github.com/elvis/ghtestrepo1.git',
+          html_url: 'https://github.com/elvis/ghtestrepo1.git',
+        },
+        {
+          id: 123123,
+          name: 'ghtestrepo2',
+          full_name: 'elvis/ghtestrepo2',
+          clone_url: 'https://github.com/elvis/ghtestrepo2.git',
+          html_url: 'https://github.com/elvis/ghtestrepo2.git',
+        },
+      ])
+
+    case 'https://gitlab.com/api/v4/projects?simple=true&min_access_level=30':
+      return toFetchResponse([
+        {
+          id: '321',
+          name: 'gltestrepo1',
+          path_with_namespace: 'elvis/gltestrepo1',
+          http_url_to_repo: 'https://github.com/elvis/gltestrepo1.git',
+          web_url: 'https://github.com/elvis/gltestrepo1.git'
+        },
+        {
+          id: '321321',
+          name: 'gltestrepo2',
+          path_with_namespace: 'elvis/gltestrepo2',
+          http_url_to_repo: 'https://github.com/elvis/gltestrepo2.git',
+          web_url: 'https://github.com/elvis/gltestrepo2.git'
+        },
+      ])
+
+    case 'https://api.bitbucket.org/2.0/repositories/elvis':
+      return toFetchResponse({
+        values: [
+          {
+            uuid: 'asdasdad',
+            name: 'bbtestrepo1',
+            full_name: 'elvis/bbtestrepo1',
+            links: {
+              html: {
+                href: 'https://bitbucket.org/elvis/bbtestrepo1'
+              },
+              clone:
+                [
+                  {
+                    name: 'https',
+                    href: 'https://bitbucket.org/elvis/bbtestrepo1.git',
+                  }
+                ]
+            },
+          },
+          {
+            uuid: 'asdasdadasd',
+            name: 'bbtestrepo2',
+            full_name: 'elvis/bbtestrepo2',
+            links: {
+              html: {
+                href: 'https://bitbucket.org/elvis/bbtestrepo2'
+              },
+              clone:
+                [
+                  {
+                    name: 'https',
+                    href: 'https://bitbucket.org/elvis/bbtestrepo2.git',
+                  }
+                ]
+            },
+          },
+        ]
       })
 
     default:
---
+++ frontend/cypress/integration/editor/repositories.spec.js
@@ -0,0 +1,46 @@
+describe('When visiting the repositories page, as a user', () => {
+  beforeEach(() => {
+    cy.deleteUser('testuser')
+    cy.createUserAndLogin('testuser', 'testuser@testus.er', 'Testi123!')
+  })
+  it('I can see appropriate message when I am logged in but not connected to any services', () => {
+    cy.visit(Cypress.env('HOST') + '/repositories')
+    cy.contains('error getting repos...User is not connected to any service')
+  })
+  it('I can see my Github repos after connecting with Github', () => {
+    cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+    cy.visit(Cypress.env('HOST') + '/repositories')
+    cy.contains('ghtestrepo1')
+    cy.contains('ghtestrepo2')
+  })
+  it('I can see my Bitbucket repos after connecting with Bitbucket', () => {
+    cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.visit(Cypress.env('HOST') + '/repositories')
+    cy.contains('bbtestrepo1')
+    cy.contains('bbtestrepo2')
+  })
+  it('I can see my Gitlab repos after connecting with Gitlab', () => {
+    cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.visit(Cypress.env('HOST') + '/repositories')
+    cy.contains('gltestrepo1')
+    cy.contains('gltestrepo2')
+  })
+
+  it('I can see all of my repos after connecting with every service', () => {
+    cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+
+    cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.visit(Cypress.env('HOST') + '/repositories')
+    cy.contains('ghtestrepo1')
+    cy.contains('ghtestrepo2')
+    cy.contains('gltestrepo1')
+    cy.contains('gltestrepo2')
+    cy.contains('bbtestrepo1')
+    cy.contains('bbtestrepo2')
+  })
+
+  afterEach(() => {
+    cy.deleteUser('testuser')
+  })
+})
