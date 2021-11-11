0b2424bb729d3f20863369bbcf10224903dad8d1
Juuso Kotimäki <jkotima@gmail.com>
2021-11-11 16:49:01 +0200

    wait for redirect in tests

--- frontend/cypress/integration/editor/connections.spec.js
+++ frontend/cypress/integration/editor/connections.spec.js
@@ -9,20 +9,32 @@ describe('When visiting the connections page, as a user', () => {
   })
   it('After connecting with Github, I can see the Github button disabled', () => {
     cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.get('#githubAuthButton').should('be.disabled')
   })
   it('After connecting with Gitlab, I can see the Gitlab button disabled', () => {
     cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.get('#gitlabAuthButton').should('be.disabled')
   })
   it('After connecting with Bitbucket, I can see the Bitbucket button disabled', () => {
     cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.get('#bitbucketAuthButton').should('be.disabled')
   })
   it('After connecting with every service, all of the buttons should be disabled', () => {
     cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.get('#githubAuthButton').should('be.disabled')
     cy.get('#gitlabAuthButton').should('be.disabled')
     cy.get('#bitbucketAuthButton').should('be.disabled')
--- frontend/cypress/integration/editor/repositories.spec.js
+++ frontend/cypress/integration/editor/repositories.spec.js
@@ -9,18 +9,24 @@ describe('When visiting the repositories page, as a user', () => {
   })
   it('I can see my Github repos after connecting with Github', () => {
     cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/repositories')
     cy.contains('ghtestrepo1')
     cy.contains('ghtestrepo2')
   })
   it('I can see my Bitbucket repos after connecting with Bitbucket', () => {
     cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/repositories')
     cy.contains('bbtestrepo1')
     cy.contains('bbtestrepo2')
   })
   it('I can see my Gitlab repos after connecting with Gitlab', () => {
     cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/repositories')
     cy.contains('gltestrepo1')
     cy.contains('gltestrepo2')
@@ -28,8 +34,14 @@ describe('When visiting the repositories page, as a user', () => {
 
   it('I can see all of my repos after connecting with every service', () => {
     cy.visit(Cypress.env('HOST') + '/auth/github/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/auth/bitbucket/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/auth/gitlab/callback?code=asdasdasd')
+    cy.url().should('contain', '/connections')
+
     cy.visit(Cypress.env('HOST') + '/repositories')
     cy.contains('ghtestrepo1')
     cy.contains('ghtestrepo2')
