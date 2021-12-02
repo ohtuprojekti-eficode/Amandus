d2608f130d1cd107caac68244ce7d6eeaa481909
gitblast <jarkko.mammela@gmail.com>
2021-12-02 11:58:13 +0200

    change login/logout redirect logic to support notifications

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,11 +1,33 @@
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import { ME } from '../graphql/queries'
 import { MeQueryResult } from '../types'
 
 const useUser = () => {
-  const { loading, error, data: user } = useQuery<MeQueryResult>(ME)
+  const { loading, error, data: user, refetch } = useQuery<MeQueryResult>(ME)
 
-  return { loading, error, user }
+  const { cache } = useApolloClient()
+
+  const clearUserFromCache = () => {
+    const current = cache.readQuery<MeQueryResult>({
+      query: ME,
+    })
+
+    cache.writeQuery({
+      query: ME,
+      data: {
+        ...current,
+        me: null,
+      },
+    })
+  }
+
+  return {
+    loading,
+    clearUserFromCache,
+    error,
+    user,
+    refetchUser: refetch,
+  }
 }
 
 export default useUser
